b85dc7765dfbc5239cbb72f2a1e0e9b37b6f75b3
anotherwan <itsanotherwan@gmail.com>
2017-01-06 22:47:53 -0500

    joined seed files into 1

--- seeds/contact.js
+++ seeds/contact.js
@@ -1,18 +1,31 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('contact').del()
+  return knex('phone_number').del()
     .then(function () {
       return Promise.all([
-        knex.table('contact').insert({
+        knex('phone_number').insert({
+          main: '647-453-2435',
+          secondary: '416-234-3664 ext.3',
+          tertiary: '(647) 234-2345'
+        }),
+        knex('contact').insert({
           first_name:'Helen',
           last_name: 'L',
           email: 'hl@example.com'
         }),
-        knex.table('contact').insert({
-          phone_number_id:
-          (knex.select('id').from('phone_number')).into('contact')
+        knex('address').insert({
+          unit_number: 3,
+          street_number: 280,
+          street_name: 'Main St',
+          city: 'Toronto',
+          province: 'Ontario',
+          postal_code: 'M3M 8M7'
         })
-      ])
-  })
+        // knex('contact_address').insert({
+        //   contact_id: 1,
+        //   address_id: 1
+        // })
+      ]);
+    });
 };
