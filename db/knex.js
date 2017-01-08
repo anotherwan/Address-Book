5333c5fdf3f509e445b6e05a660b722b72e88e16
anotherwan <itsanotherwan@gmail.com>
2017-01-07 23:05:22 -0500

    added express route to home, added home.ejs file with bootstrap and font-awesome

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,3 @@
-var environment = process.env.DATABASE_URL || 'development';
-var config = require('../knexfile')[environment];
-var knex = require('knex')(config);
-
-module.exports = knex;
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile')[environment];
+module.exports = require('knex')(config);
--- server.js
+++ server.js
@@ -1,22 +1,93 @@
+'use strict'
+
 const express = require('express');
 const app = express();
+const knex = require('knex');
 const PORT = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.end("Welcome to EXPRESS!");
+
+app.set('view engine', 'ejs');
+
+const contactEntryTable = [
+  {
+    id: 1,
+    first_name: 'Joe',
+    last_name: 'Bozo',
+    email: 'joe@bozo.com'
+  },
+  { id: 2,
+    first_name: 'Mary',
+    last_name: 'Kate',
+    email: 'm@k.com'
+  }
+];
+
+const phoneNumberTable = [
+  {
+    id: 1,
+    main: '647-987-6543',
+    secondary: '416-987-6543',
+    tertiary: '450-987-6543 ext.2',
+    other: '514-987-6543'
+  },
+  {
+    id: 2,
+    main: '416-010-1010',
+    secondary: '647-010-1010',
+    tertiary: '514-010-1010'
+  }
+
+];
+
+const addressEntryTable = [
+    {
+      id: 1,
+      street_number: '420',
+      street_name: 'Baker Rd',
+      city: 'Toronto',
+      province: 'Ontario',
+      postal_code: 'M2M 2M2'
+    },
+    {
+      id: 2,
+      unit_number: '2',
+      street_number: '468',
+      street_name: 'Sunset Blvd',
+      city: 'Montreal',
+      province: 'Quebec',
+      postal_code: 'H1H 1H1'
+    }
+];
+
+app.get('/contacts', (req, res) => {
+  res.render('home', { contact: contactEntryTable} )
 });
 
-// let contactsDatabase = {
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
+
+// app.get('/contacts', (req, res) => {
+//   knex('contact')
+//     .select('*')
+//     .then(contact => {
+//       res.render('home', {
+//         first_name: contact.first_name,
+//         last_name: contact.last_name,
+//         email: contact.email
+//       });
+//     })
+// });
+
+// get shit from db
+
+// put in obj
+
+// app.get('/contacts/:contactId', (req, res) => {
+//   res.render('')
+// })
 //
-// }
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/contacts", (req, res) => {
-  let templateVars = { contacts: contactsDatabase };
-  res.render("contacts_index", templateVars);
-});
+// app.post('/contacts/new', (req, res) => {
+//
+// })
